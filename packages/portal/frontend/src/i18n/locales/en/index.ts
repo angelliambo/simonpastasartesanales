25b503f039bb3653ee3b2f1395ad0f07ceca42fd
Angel Adrian Liambo <aliambo@jobint.com>
2026-07-10 23:52:41 -0300

    refactor: modularize i18n locale exports and bump package versions

--- packages/portal/frontend/src/i18n/locales/en/index.ts
+++ packages/portal/frontend/src/i18n/locales/en/index.ts
@@ -1,7 +1,36 @@
-import * as commonsPages from '../../pages/commons/en';
-import * as portalPages from '../../pages/portal/en';
+import brandSlogan from '../../pages/commons/en/brandSlogan';
+import emails from '../../pages/commons/en/emails';
+import legal from '../../pages/commons/en/legal';
+import privacy from '../../pages/commons/en/privacy';
 
-const pages = { ...commonsPages, ...portalPages };
+import home from '../../pages/portal/en/home';
+import loginRegister from '../../pages/portal/en/login';
+import errors from '../../pages/portal/en/errors';
+import navigation from '../../pages/portal/en/navigation';
+import cookie from '../../pages/portal/en/cookie';
+import notFound from '../../pages/portal/en/notFound';
+import dashboard from '../../pages/portal/en/dashboard';
+import admin from '../../pages/portal/en/admin';
+import support from '../../pages/portal/en/support';
+
+const { login, register } = loginRegister;
+
+const pages = {
+  brandSlogan,
+  emails,
+  legal,
+  privacy,
+  home,
+  login,
+  register,
+  errors,
+  navigation,
+  cookie,
+  notFound,
+  dashboard,
+  admin,
+  support,
+};
 
 export default pages;
 export { pages };
--- packages/portal/frontend/src/i18n/locales/es/index.ts
+++ packages/portal/frontend/src/i18n/locales/es/index.ts
@@ -1,7 +1,36 @@
-import * as commonsPages from '../../pages/commons/es';
-import * as portalPages from '../../pages/portal/es';
+import brandSlogan from '../../pages/commons/es/brandSlogan';
+import emails from '../../pages/commons/es/emails';
+import legal from '../../pages/commons/es/legal';
+import privacy from '../../pages/commons/es/privacy';
 
-const pages = { ...commonsPages, ...portalPages };
+import home from '../../pages/portal/es/home';
+import loginRegister from '../../pages/portal/es/login';
+import errors from '../../pages/portal/es/errors';
+import navigation from '../../pages/portal/es/navigation';
+import cookie from '../../pages/portal/es/cookie';
+import notFound from '../../pages/portal/es/notFound';
+import dashboard from '../../pages/portal/es/dashboard';
+import admin from '../../pages/portal/es/admin';
+import support from '../../pages/portal/es/support';
+
+const { login, register } = loginRegister;
+
+const pages = {
+  brandSlogan,
+  emails,
+  legal,
+  privacy,
+  home,
+  login,
+  register,
+  errors,
+  navigation,
+  cookie,
+  notFound,
+  dashboard,
+  admin,
+  support,
+};
 
 export default pages;
 export { pages };
