581d1af92fc833bab6573d42a6c8243a036290b1
angelliambo <aliambo@jobint.com>
2026-09-12 22:30:25 -0300

    refactor: remove barrel export files and reorganize common types and imports

--- packages/portal/frontend/src/services/api/index.ts
+++
@@ -1,7 +0,0 @@
-export { apiClient } from "./client";
-export * from "./authService";
-export * from "./userService";
-export * from "./adminService";
-export * from "./licenseService";
-export * from "./socialFeedService";
-export * from "./systemService";
